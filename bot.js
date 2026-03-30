81167496f4c9c90303269b7fda0825c548e2e724
Harryofa <126290539+Harryofa@users.noreply.github.com>
2026-03-30 11:36:01 +0100

    bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,6 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
 puppeteer.use(StealthPlugin());
 
-const PROXY = "http://123.45.67.89:8080"; // PUT YOUR PROXY HERE
-
 async function scrape() {
   let browser;
 
@@ -14,19 +12,12 @@ async function scrape() {
     browser = await puppeteer.launch({
       headless: true,
       args: [
-        `--proxy-server=${PROXY}`,
         "--no-sandbox",
         "--disable-setuid-sandbox"
       ]
     });
 
     const page = await browser.newPage();
-
-    // ONLY use this if your proxy has username/password
-    // await page.authenticate({
-    //   username: "your_username",
-    //   password: "your_password"
-    // });
 
     await page.goto(
       "https://m.betking.com/en-ng/virtuals/scheduled/leagues/kings-league",
@@ -42,13 +33,9 @@ async function scrape() {
 
   } catch (err) {
     console.log("❌ ERROR:", err.message);
-
     if (browser) await browser.close();
   }
 }
 
-// Run every 3 minutes
 setInterval(scrape, 180000);
-
-// Run immediately
 scrape();
